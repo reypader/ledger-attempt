f4ce19feb567e696b198574f9f0a64b188c76ad2
Reynald Pader <rey.pader@paymaya.com>
2021-02-13 19:02:45 +0800

    Add account projection tables in init.sql script

--- init.sql
+++ init.sql
@@ -1,3 +1,45 @@
+-- LEDGER PROJECTION TABLES
+CREATE TABLE IF NOT EXISTS public.ledger_account_info(
+    account_id VARCHAR(255) NOT NULL,
+    mode VARCHAR(10) NOT NULL
+    PRIMARY KEY(account_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.ledger_account_tags(
+    id BIGSERIAL,
+    account_id VARCHAR(255) NOT NULL,
+    tag VARCHAR(255) NOT NULL
+    PRIMARY KEY(account_id, tag),
+        CONSTRAINT fk_account_tag_account_info
+          FOREIGN KEY(account_id)
+          REFERENCES ledger_account_info(account_id)
+          ON DELETE CASCADE
+
+);
+CREATE INDEX idx_account_tags_1 on public.ledger_account_tags (tag)
+
+CREATE TABLE IF NOT EXISTS public.ledger_account_statement(
+    id BIGSERIAL,
+    account_id VARCHAR(255) NOT NULL,
+    entry_id VARCHAR(255) NOT NULL,
+    entry_code VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    available_balance_change DECIMAL(18,2) NOT NULL,
+    current_balance_change DECIMAL(18,2) NOT NULL,
+    resulting_available_balance DECIMAL(18,2) NOT NULL,
+    resulting_current_balance DECIMAL(18,2) NOT NULL
+    authorized_on TIMESTAMP WITH TIMEZONE NOT NULL,
+    posted_on TIMESTAMP WITH TIMEZONE NOT NULL,
+    PRIMARY KEY(id),
+        CONSTRAINT fk_account_statement_account_info
+          FOREIGN KEY(account_id)
+          REFERENCES ledger_account_info(account_id)
+          ON DELETE CASCADE
+
+);
+CREATE INDEX idx_account_statement_1 on public.ledger_account_statement (entry_id, account_id)
+
+-- AKKA PERSISTENCE TABLES
 CREATE TABLE IF NOT EXISTS public.event_journal(
   ordering BIGSERIAL,
   persistence_id VARCHAR(255) NOT NULL,
